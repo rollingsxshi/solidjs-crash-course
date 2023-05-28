5d30c884d8e14ce3b46ef14a99d6a4ac4f9fb4bb
therollingambit <therollingambit@gmail.com>
2023-05-28 16:15:00 +0800

    props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import styles from './App.module.css';
-import banner from './assets/banner.png'
-import Card from './components/Card';
+import styles from "./App.module.css";
+import banner from "./assets/banner.png";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div class='container m-auto'>
+    <div class="container m-auto">
       <header>
         <h1>Ninja Merch</h1>
       </header>
 
-      <img class='rouned-md' src={banner} alt="site banner" />
+      <img class="rouned-md" src={banner} alt="site banner" />
 
-      <div class='grid grid-cols-4 gap-10 my-4'>
+      <div class="grid grid-cols-4 gap-10 my-4">
         {/* <Card title='ninja tee' /> */}
-        <Card title='ninja tote bag' rounded={true} flat={false} />
-        <Card title='ninja hoodie' rounded={false} flat={true} />
+        <Card rounded={true} flat={false}>
+          <h2>Ninja Tee, Black</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore,
+            vel iure? Velit id nam tenetur?
+          </p>
+          <button class="btn">view</button>
+        </Card>
+        <Card rounded={false} flat={true}>
+          <h2>Ninja Tee, White</h2>
+          <button class="btn">view</button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore,
+            vel iure? Velit id nam tenetur?
+          </p>
+          <p>Only $10</p>
+        </Card>
       </div>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,7 @@ const Card = (props) => {
       class="bg-white p-4 text-center"
       classList={ {'rounded-md': props.rounded, 'shadow-md': !props.flat} }
     >
-      <h2>{ props.title }</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, vel
-        iure? Velit id nam tenetur?
-      </p>
-      <button class="btn">click me!</button>
+      {props.children}
     </div>
   );
 };
