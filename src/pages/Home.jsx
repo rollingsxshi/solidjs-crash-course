f54530dc858c074cae5eed8581bb71a0d41c4189
therollingambit <therollingambit@gmail.com>
2023-05-28 18:22:42 +0800

    conditional rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,27 @@
-import { createResource } from "solid-js";
+import { For, Show, createResource } from "solid-js";
 import Card from "../components/Card";
 
 const fetchProducts = async () => {
-  const res = await fetch('https://fakestoreapi.com/products')
-  return res.json()
-}
+  const res = await fetch("https://fakestoreapi.com/products");
+  return res.json();
+};
 
 function Home() {
   const [products] = createResource(fetchProducts);
 
   return (
-    <div class="grid grid-cols-4 gap-10 my-4">
-      <Card rounded={true} flat={false}>
-        <h2>Ninja Tee, Black</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, vel
-          iure? Velit id nam tenetur?
-        </p>
-        <button class="btn">view</button>
-      </Card>
-      <Card rounded={false} flat={true}>
-        <h2>Ninja Tee, White</h2>
-        <button class="btn">view</button>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, vel
-          iure? Velit id nam tenetur?
-        </p>
-        <p>Only $10</p>
-      </Card>
-
-      <p>{ console.log(products(), products.loading) }</p>
-    </div>
+    <Show when={products()} fallback={<p>Loading...</p>}>
+      <div class="grid grid-cols-4 gap-10 my-4">
+        <For each={products()}>
+          {(product) => (
+            <Card rounded={true} flat={true}>
+              <img src={ product.image } alt="product img" />
+              <h2 class="my-3 font-bold">{ product.title }</h2>
+            </Card>
+          )}
+        </For>
+      </div>
+    </Show>
   );
 }
 export default Home;
